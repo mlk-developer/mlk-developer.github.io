54bc5bebb220ee4e84bde65e4158194cb832e1d8
Mohamed <m.kajeiou123@gmail.com>
2020-07-24 12:57:49 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+$("#title").css({
+    "opacity": "0",
+    "marginLeft": "-=100"
+})
+$("p").css({
+    "opacity": "0",
+    "marginLeft": "-=100"
+})
+$("h3").css({
+    "opacity": "0",
+    "marginLeft": "-=100"
+})
+$("a").css({
+    "opacity": "0",
+    "marginLeft": "-=100"
+})
+$("h5").css({
+    "opacity": "0",
+    "marginLeft": "-=100"
+})
+
+$("img").css({
+    "opacity": 0
+})
+
+
+$("#title").animate({
+    marginLeft: "+=100",
+    opacity: 1,
+}, 1000, function () {
+    $("h3").animate(
+        {
+            opacity: "1.0",
+            marginLeft: "+=100",
+        }, 500
+    );
+    $("p").animate(
+        {
+            opacity: "1.0",
+            marginLeft: "+=100",
+        }, 500, function () {
+            $("img").animate({
+                opacity: "1.0"
+            }, 1000)
+        }
+    );
+    $("a").animate(
+        {
+            opacity: "1.0",
+            marginLeft: "+=100",
+        }, 500);
+
+    $("h5").animate(
+        {
+            opacity: "1.0",
+            marginLeft: "+=100",
+        }, 500);
+});
+
+$("#btnDark").click(function () {
+    $("body").css('background', 'rgb(0,0,0)');
+
+    $("p").css('color', 'rgba(200,200,200)')
+    $("h1").css('color', 'rgba(200,200,200)')
+    $("h3").css('color', 'rgba(200,200,200)')
+
+})
+
+$("#btnLight").click(function () {
+    $("body").css('background', '#eaeaea');
+
+    $("p").css('color', '#797979')
+    $("h1").css('color', '#797979')
+    $("h3").css('color', '#797979')
+
+})
